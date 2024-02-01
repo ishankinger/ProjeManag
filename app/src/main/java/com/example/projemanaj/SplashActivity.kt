2db66077e1ea06cc17b746dc0a15f6d840379d2a
ishan420 <kingerishan@gmail.com>
2024-02-01 18:00:03 +0530

    Add Intro Activity and Splash time navigation

--- app/src/main/java/com/example/projemanaj/SplashActivity.kt
+++ app/src/main/java/com/example/projemanaj/SplashActivity.kt
@@ -1,7 +1,9 @@
 package com.example.projemanaj
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
@@ -14,5 +16,12 @@ class SplashActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        // Navigation from splash Activity to intro Activity after a certain period of time
+        Handler().postDelayed({
+            startActivity(Intent(this,IntroActivity::class.java))
+            // this finish function help user not come back to splash Activity after pressing back button
+            finish()
+        },2500)
     }
 }
