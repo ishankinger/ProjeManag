44f85dfe0f70ab24a02bc565aa78b998ae030571
ishan420 <kingerishan@gmail.com>
2024-02-05 18:55:15 +0530

    Adding Sign Out and My Profile Options

--- app/src/main/java/com/example/projemanaj/activities/MainActivity.kt
+++ app/src/main/java/com/example/projemanaj/activities/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.projemanaj.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.text.Layout
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.projemanaj.R
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private var doubleBackToSignOutPressedOnce = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupActionBar()
+        findViewById<NavigationView>(R.id.nav_view).setNavigationItemSelectedListener(this)
     }
     private fun setupActionBar(){
         val toolbar : Toolbar = findViewById(R.id.toolbar_main_activity)
@@ -41,5 +50,32 @@ class MainActivity : BaseActivity() {
         else{
             doubleBackToExit()
         }
+    }
+
+    // Called when an item in the navigation menu is selected.
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_sign_out ->{
+                doubleBackToSignOut()
+            }
+            R.id.nav_my_profile ->{
+                Toast.makeText(this, "Profile was clicked", Toast.LENGTH_LONG).show()
+                findViewById<DrawerLayout>(R.id.drawer_layout).closeDrawer(GravityCompat.START)
+            }
+        }
+        return true
+    }
+
+    private fun doubleBackToSignOut(){
+        if(doubleBackToSignOutPressedOnce) {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this,IntroActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            return
+        }
+        doubleBackToSignOutPressedOnce = true
+        showErrorSnackBar("Sure You want to Exit then Double Press")
+        Handler().postDelayed({doubleBackToSignOutPressedOnce = false},4000)
     }
 }
